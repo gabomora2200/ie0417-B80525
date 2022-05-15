2e8e82919af0c6b9b60da9738c9889d8517ddd30
Andres Arias <andres.ariascampos@ucr.ac.cr>
2022-05-15 12:02:52 -0600

    Aplicando los  cambios al API

--- labs_src/lab2/eieManager/lib/eieManager/APIServer/main.py
+++ labs_src/lab2/eieManager/lib/eieManager/APIServer/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body
 from typing import Optional, List
 from pydantic import BaseModel
+from ..DeviceManager import DeviceManager
 import json
 
 class Device(BaseModel):
@@ -11,18 +12,19 @@ class Device(BaseModel):
 
 class Command(BaseModel):
     device_id: str
-    command: str
+    cmd: str
     args: Optional[List[str]] = None
 
 app = FastAPI()
 
+device_manager = DevMan()
 
+'''
 config = [
     ("Dev-01", "Sensor", ["Read"], "192.168.0.2:8000"),
     ("Dev-02", "Display", ["Status", "Set_device"], "192.168.0.3:8000"),
 ]
 
-#Pasar los return a un diccionario
 
 devices_inv = {
     id: Device(
@@ -33,14 +35,14 @@ devices_inv = {
     )
     for id, subdevice, lists, info in config
 }
-
+'''
 
 @app.post("/devices/")
 def create_device(device: Device):
     """
     Create a device and register it
     """
-    #out = json.load(device)
+    device_manager.create_device(device.id, device.types, device.commands, device.net_info)
     devices_inv[device.id] = device
     return device
 
@@ -50,7 +52,8 @@ def get_command(command: Command):
     """
     Send a command and their info
     """
-    commands[command.name] = command
+    device_manager.get_command(command.device_id, command.cmd, command.args)
+    #commands[command.name] = command
     return command
 
 
@@ -59,6 +62,7 @@ def update_device(device: Device):
     """
     Update a device
     """
+    device_manager.create_device(device.id, device.types, device.commands, device.net_info)
     devices_inv[device.id] = device
     return device
 
@@ -67,6 +71,7 @@ def delete_device(device_name: str, status_code=204):
     """
     Unregister and delete a device.
     """
+    device_manager.delete_device(device.id, device.types, device.commands, device.net_info)
     del devices_inv[device_name]
 
 @app.get("/devices/")
@@ -74,13 +79,15 @@ def devices_info():
     """
     Get the information of all devices
     """
-    devices_list = [device for device in devices_inv.values()]
-    return devices_list
+    info = device_manager.devices_info()
+    #devices_list = [device for device in devices_inv.values()]
+    return json.dump(info)
 
 @app.get("/devices/{device_name}")
 def device_info(device_name: str):
     """
     Get the information of a device
     """
-    return devices_inv[device_name]
+    info = device_manager.device_info(device_name)
+    return info
 
