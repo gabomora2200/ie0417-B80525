cad25f574502cb5c6db07f2ddd695c5d55a5cb11
Erick Sancho <alonso.sancho99@gmail.com>
2022-06-18 00:56:08 -0600

    Tests 2, 5 y 6
    ademas se corrige un error en el Mock sensor type

--- labs_src/lab4/python/sensor_commands/test/test_sensor_manager.py
+++ labs_src/lab4/python/sensor_commands/test/test_sensor_manager.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import fixture
 import logging
 from sensor_commands.sensor.manager import SensorManager
@@ -26,12 +27,44 @@ def sensor_mgr(pytestconfig):
     smgr = SensorManager(pytestconfig.getoption("conf_dir"))
     return smgr
 
+# -----------------------------------------------------------------
+# Tests
+# -----------------------------------------------------------------
+
+# Test 1
+
 
 def test_sensor_manager_supported_types(sensor_mgr):
     sensor_name_list = sensor_mgr.get_supported_sensor_types()
     assert len(sensor_name_list) > 0, "Sensor name list length = 0"
 
+# Test 2
 
+
+def test_sensor_manager_single_sensor_create_destroy(fixt_ctx, sensor_mgr):
+    sensor_name = fixt_ctx.rng.get_rand_list_item(fixt_ctx.names)
+    sensor_type = fixt_ctx.rng.get_rand_list_item(fixt_ctx.types)
+    print("sensor name: ", sensor_name, "\nsensor type: ", sensor_type)
+    try:
+        sensor_mgr.create_sensor(sensor_name, sensor_type)
+        pass_flag = 1
+    except BaseException:
+        pass_flag = 0
+    assert pass_flag == 1, "Can't Create Sensor"
+
+    assert sensor_name in sensor_mgr.get_sensors_info(), "Could Create sensor"
+
+    with pytest.raises(AssertionError):
+        sensor_mgr.create_sensor(sensor_name, sensor_type)
+
+    sensor_mgr.destroy_sensor(sensor_name)
+    assert sensor_name not in sensor_mgr.get_sensors_info(), "Could not Destroy sensor"
+
+    with pytest.raises(AssertionError):
+        sensor_mgr.destroy_sensor(sensor_name)
+
+
+# Test 3
 def test_sensor_manager_single_sensor_read_command(fixt_ctx, sensor_mgr):
     sensor_name = fixt_ctx.rng.get_rand_list_item(fixt_ctx.names)
     sensor_type = fixt_ctx.rng.get_rand_list_item(fixt_ctx.types)
@@ -61,6 +94,8 @@ def test_sensor_manager_single_sensor_read_command(fixt_ctx, sensor_mgr):
     for i in sensor_mgr.get_sensors_info():
         assert i != sensor_name, "Could not Destroy sensor"
 
+# Test 4
+
 
 def test_sensor_manager_mock_type_register_unregister(sensor_mgr):
     sensor_mgr.register_sensor_type("mock", MockSensor)
@@ -71,3 +106,56 @@ def test_sensor_manager_mock_type_register_unregister(sensor_mgr):
     sensor_name_list = sensor_mgr.get_supported_sensor_types()
     print(sensor_name_list)
     assert "mock" not in sensor_name_list, "mock is still a sensor type"
+
+
+# Test 5
+def test_sensor_manager_mock_sensor_create_destroy(fixt_ctx, sensor_mgr):
+    sensor_mgr.register_sensor_type("mock", MockSensor)
+    assert "mock" in sensor_mgr.get_supported_sensor_types(), "mock is not a sensor type"
+
+    sensor_name = fixt_ctx.rng.get_rand_list_item(fixt_ctx.names)
+
+    try:
+        sensor_mgr.create_sensor(sensor_name, "mock")
+        pass_flag = 1
+    except BaseException:
+        pass_flag = 0
+    assert pass_flag == 1, "Can't Create Sensor"
+
+    assert sensor_name in sensor_mgr.get_sensors_info(), "Could Create sensor"
+    assert sensor_mgr.get_sensors_info()[sensor_name]["type"] == 'mock', "Sensor Type error"
+
+    sensor_mgr.destroy_sensor(sensor_name)
+    assert sensor_name not in sensor_mgr.get_sensors_info(), "Could not Destroy sensor"
+
+    sensor_mgr.unregister_sensor_type("mock")
+    assert "mock" not in sensor_mgr.get_supported_sensor_types(), "mock is still a sensor type"
+
+# Test 6
+
+
+def test_sensor_manager_mock_sensor_read_command(fixt_ctx, sensor_mgr):
+    sensor_mgr.register_sensor_type("mock", MockSensor)
+    assert "mock" in sensor_mgr.get_supported_sensor_types(), "mock is not a sensor type"
+
+    sensor_name = fixt_ctx.rng.get_rand_list_item(fixt_ctx.names)
+
+    try:
+        sensor_mgr.create_sensor(sensor_name, "mock")
+        pass_flag = 1
+    except BaseException:
+        pass_flag = 0
+    assert pass_flag == 1, "Can't Create Sensor"
+
+    read_cmd = sensor_mgr.create_sensor_read_cmd(sensor_name)
+    assert read_cmd is not None, "Invalid sensor read command creation"
+
+    read_cmd.execute()
+
+    sensor_mgr.sensors[sensor_name].assert_read()
+
+    sensor_mgr.destroy_sensor(sensor_name)
+    assert sensor_name not in sensor_mgr.get_sensors_info(), "Could not Destroy sensor"
+
+    sensor_mgr.unregister_sensor_type("mock")
+    assert "mock" not in sensor_mgr.get_supported_sensor_types(), "mock is still a sensor type"
