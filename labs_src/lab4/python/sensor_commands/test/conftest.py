20bb2ebbad01c833bf0444b3ab5dfe1b7e4c91bf
gabo <gabomora2200@gmail.com>
2022-06-17 22:11:06 -0700

    Correccion de errores y agregando test #4

--- labs_src/lab4/python/sensor_commands/test/conftest.py
+++ labs_src/lab4/python/sensor_commands/test/conftest.py
@@ -1,27 +1,10 @@
-from pytest import fixture
-import os
 # NOTE: The fixtures here will be available for all tests.
 # For more information about fixtures and conftest.py refer to:
 # https://docs.pytest.org/en/7.1.x/how-to/fixtures.html
 
 
 def pytest_addoption(parser):
+    """
+    function used to get config dir for test env
+    """
     parser.addoption("--conf_dir", action="store", default="default conf_dir")
-
-
-@fixture(scope='function')
-def conf_filename():
-    print("ENV VAR: ", os.environ.get('conf_dir'))
-    return "/home/gabriel/Desktop/ie0417-B80525/labs_src/lab4/python/sensor_commands/config/sensors_cfg.json"
-
-
-'''
-@fixture(scope='function')
-def example_func_fixt():
-    logging.info("This is a function-scope example fixture!")
-    data = {
-        "name": "Data",
-        "list": ["a", "b", "c"],
-    }
-    return data
-    '''
