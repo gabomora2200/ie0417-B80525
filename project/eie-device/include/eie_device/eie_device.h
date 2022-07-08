702c37ea7b5a852ab42a49278c6ec07cd3162fc0
Erick Sancho <alonso.sancho99@gmail.com>
2022-07-07 21:46:24 -0600

    Base de eie-device, API
    Se agrega el header principal eie_device.h, y las funciones del API.
    Ademas de una implementacion base del cuerpo de dichas funciones.

--- project/eie-device/include/eie_device/eie_device.h
+++ project/eie-device/include/eie_device/eie_device.h
@@ -4,23 +4,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*****************************/
+/*     Structs and enums     */
+/*****************************/
 /**
  * @brief This structure contain the basic data of the thing 
  * 
  */
 typedef struct {
     int thing_id;
-    features feat;
+    char * namespace;
 } device;
 
 /**
- * @brief feature structure
+ * @brief 
  * 
  */
 typedef struct
 {
-    /* data */
-} features;
+    char * feature_id;
+    void * clbk_fn;
+} feature_clbk;
 
 
 /*****************************/
@@ -41,19 +45,34 @@ device * create_device(char *cfg_json, char * namespace);
  * @brief Destroy Device "object"
  * 
  * @param dev  Device object
+ * @return int Fail or pass
  */
-void destroy_device(device *dev);
+int destroy_device(device *dev);
 
 /**
  * @brief 
  * 
  * @param clbk_fn 
- * @param feature_name 
- * @param json_data 
+ * @param feature_id 
  * @return int 
  */
-int register_callback(void * clbk_fn, char * feature_name, char * json_data);
+int register_callback(feature_clbk * clbk);
 
+/**
+ * @brief update a feature
+ * 
+ * @param feature_id 
+ * @param data 
+ * @return int 
+ */
+int update_feature(char * feature_id, char * data);
 
+/**
+ * @brief Add or delete a feature
+ * 
+ * @param feature_id 
+ * @return int 
+ */
+int modify_config(char * feature_id);
 
 #endif //EIE_DEVICE_H
