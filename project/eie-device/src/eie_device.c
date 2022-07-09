f903588d24a28cd0e8324b79b8c76d78f7e6148a
Erick Sancho <alonso.sancho99@gmail.com>
2022-07-08 23:32:02 -0600

    Definicion de callback y callback manager.
    Se agrega la logica encargada de crear, almacenar, ejecutar y destruir los callbacks.
    Ademas se integra esta logica en las funciones del API. Las cuales son: create_device(),
    destoy_device(), y register_device();

--- project/eie-device/src/eie_device.c
+++ project/eie-device/src/eie_device.c
@@ -1,24 +1,47 @@
 #include "eie_device/eie_device.h"
+#include "eie_device/callback_manager/callback_manager.h"
+
+
+struct device{
+    int thing_id;
+    char * namespace;
+    callback_manager * clkb_mgr;
+};
 
 
 // Create device definition 
-device * create_device(char *cfg_json,  char * namespace){
-    device * dev = malloc(sizeof(device));
-    dev->thing_id = 20;
+struct device * eie_device_create(char *cfg_json,  char * namespace){
+    struct device * dev = malloc(sizeof(struct device));
+    
+    /** Creat the callback manager */
+    dev->clkb_mgr = callback_manager_create();
+
     return dev;
 }
 
 // Destroy a device
-int destroy_device(device *dev){
-    return 1;
+eie_status eie_device_destroy(struct device *dev){
+    callback_manager_destroy(dev->clkb_mgr);
+    free(dev);
+    return OK;
 }
 
 //Register a Callback
-int register_callback(feature_clbk * clbk){
-    return 1;
+eie_status eie_device_register_callback(struct device * dev, char * feature_id,
+        eie_callback_execute_t clbk_fn){
+    int ret;
+    ret = callback_manager_add_clbk(dev->clkb_mgr, feature_id, clbk_fn);
+    if(ret){
+        return ERROR;
+    }
+    return OK;
 }
 
 // Update status feature
-int update_feature(char * feature_id, char * data){
-    return 1;
+eie_status eie_device_update_feature(struct device * dev, char * feature_id, char * data){
+    return OK;
+}
+
+eie_status eie_device_modify_config(struct device * dev, char * feature_id){
+    return OK;
 }
