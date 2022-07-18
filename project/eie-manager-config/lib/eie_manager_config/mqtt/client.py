01ba51e020e0b6254e734f4dab5f741d33c738b2
Andres Arias <andres.ariascampos@ucr.ac.cr>
2022-07-17 23:20:32 -0600

    Subiendo avances de MQTT

---
+++ project/eie-manager-config/lib/eie_manager_config/mqtt/client.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt 
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        client.subscribe('v1/devices/me/rpc/request/+')
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+        client.loop_stop()  
+
+def on_disconnect(client, userdata, rc):
+   print("client disconnected ok")
+
+def on_publish(client, userdata, mid):
+    print("In on_pub callback mid= "  ,mid)
+
+
+def MQTT_publish(topic: str, payload: str):
+    try:
+        client.publish(topic, payload)
+    except:
+        print("Error at publish message")
+
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+
+broker_address="172.17.0.1:1883"
+
+client = mqtt.Client("MQTT_conection") #create new instance
+
+client.connect(broker_address) #connect to broker
+
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+
+
+
+while (1):
+    client.loop()
