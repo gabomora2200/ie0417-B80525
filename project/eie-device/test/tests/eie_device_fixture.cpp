c74c71f9fd14c74cb401c1f8288a943fd805c2c1
gabo <gabomora2200@gmail.com>
2022-07-27 16:58:49 -0700

    agregando cambios finales a los test

---
+++ project/eie-device/test/tests/eie_device_fixture.cpp
@@ -0,0 +1,82 @@
+#include "gtest/gtest.h"
+
+#include <eie_device/eie_device.h>
+#include <testutil/rand_gen.hpp>
+
+
+// Creando mock functions
+void mock_eie_callback_fn(char* payload, int payloadlen){
+    std::cout << payload << std::endl;
+}
+
+/** Example fixture class for demo API tests */
+class eie_device_fixture : public testing::Test
+{
+ protected:
+    /* Fixture class members, accesible from test functions */
+    int value;
+    testutil::rand_gen rng;
+    struct device * dev;
+
+    /* Fixture class constructor */
+    /* NOTE: Using reproducible random value for seed, check
+     * explanation in unittest_main.cpp for more details */
+    eie_device_fixture()
+        : value(2), rng(rand())
+    {
+        std::cout << "EIE DEVICE Test fixture constructor! "<< std::endl;
+        std::cout << "  RNG seed " << rng.get_seed() << std::endl;
+    }
+
+    virtual void SetUp() {
+        std::cout << "EIE DEVICE Test fixture SetUp! "<< std::endl;
+        char cfg_json[] = "{\"namespace\":\"sensor\",\"features\":{\"ft_1\":{\"properties\":{\"configuration\":{\"ON\":true},\"status\":{\"value\":0}}}}}";
+        srand(rng.get_seed());
+        dev = eie_device_create(cfg_json);
+        ASSERT_NE(dev, nullptr);
+    }
+
+    virtual void TearDown() {
+        std::cout << "EIE DEVICE Test fixture TearDown! \n"<< std::endl;
+        eie_device_destroy(dev);
+    }
+};
+
+/** Test the functions create and destroy of command runner using a fixture */
+TEST_F(eie_device_fixture, eie_device_register_callback_test)
+{
+    eie_status ret = OK; 
+    char ftID[] = "ft_1";
+    eie_callback_execute_t clbkfn = (eie_callback_execute_t)mock_eie_callback_fn;
+    ret = eie_device_register_callback(dev, ftID, clbkfn);
+    ASSERT_EQ(ret, OK);
+}
+
+TEST_F(eie_device_fixture, eie_device_re_register_callback_test)
+{
+    eie_status ret; 
+    char ftID[] = "ft_1";
+    eie_callback_execute_t clbkfn = (eie_callback_execute_t)mock_eie_callback_fn;
+    ret = eie_device_register_callback(dev, ftID, clbkfn);
+    ASSERT_EQ(ret, OK);
+
+    ret = eie_device_register_callback(dev, ftID, clbkfn);
+    ASSERT_EQ(ret, ERROR);
+}
+
+TEST_F(eie_device_fixture, eie_device_update_feature_test)
+{
+    eie_status ret; 
+    char ftID[] = "ft_1";
+    char data_update[] = "{\"value\":9}";
+    ret = eie_device_update_feature(dev, ftID, data_update);
+    ASSERT_EQ(ret, OK);
+}
+
+TEST_F(eie_device_fixture, eie_device_modify_config_test)
+{
+    eie_status ret; 
+    char ftID[] = "ft_1";
+    ret = eie_device_modify_config(dev, ftID);
+    ASSERT_EQ(ret, OK);
+}
---
+++ project/eie-device/test/tests/mqtt_client_fixture.cpp
@@ -0,0 +1,78 @@
+#include "gtest/gtest.h"
+#include "MQTTClient.h"
+#include "time.h"
+
+#include <eie_device/eie_device.h>
+#include <eie_device/callback_manager/callback_manager.h>
+#include <eie_device/callback_manager/callback.h>
+#include <testutil/rand_gen.hpp>
+#include <eie_device/mqtt_client/mqtt_client.h>
+
+/** Example fixture class for demo API tests */
+class mqtt_client_fixture : public testing::Test
+{
+ protected:
+    /* Fixture class members, accesible from test functions */
+    int value;
+    testutil::rand_gen rng;
+    callback_manager *clbk_mgr;
+    MQTTClient *mqtt_client;
+
+    /* Fixture class constructor */
+    /* NOTE: Using reproducible random value for seed, check
+     * explanation in unittest_main.cpp for more details */
+    mqtt_client_fixture()
+        : value(2), rng(rand())
+    {
+        std::cout << "MQTT CLIENT Test fixture constructor! "<< std::endl;
+        std::cout << "  RNG seed " << rng.get_seed() << std::endl;
+    }
+
+    virtual void SetUp() {
+        std::cout << "MQTT CLIENT Test fixture SetUp! "<< std::endl;
+        srand(rng.get_seed());
+        clbk_mgr = callback_manager_create();
+        ASSERT_NE(clbk_mgr, nullptr);
+        mqtt_client = MQTT_client_create(clbk_mgr, "123", (void*)("sensor:0"));
+        ASSERT_NE(mqtt_client, nullptr);
+    }
+
+    virtual void TearDown() {
+        std::cout << "MQTT CLIENT Test fixture TearDown! \n"<< std::endl;
+        MQTT_client_destroy(mqtt_client);
+        callback_manager_destroy(clbk_mgr);
+    }
+};
+
+/** Test the functions create and destroy of command runner using a fixture */
+TEST_F(mqtt_client_fixture, valid_publish)
+{
+    int ret; 
+    char topic[] = "eie-manager/sensor:0";
+    char message[] = "{\"topic\":\"/sensor:0/things/twin/commands/modify\",\"path\":\"/features/ft_1/properties/status\",\"value\":{\"value\":9}}";
+    ret = MQTT_publish(mqtt_client, topic, message);
+    ASSERT_EQ(ret, 1);
+}
+
+TEST_F(mqtt_client_fixture, invalid_publish)
+{
+    int ret; 
+    char message[] = "eie-device/Test Publish";
+    ret = MQTT_publish(mqtt_client, NULL, message);
+    ASSERT_EQ(ret, 0);
+}
+
+TEST_F(mqtt_client_fixture, valid_subscribe)
+{
+    int ret; 
+    char topic[] = "eie-manager/sensor:0";
+    ret = MQTT_subscribe(mqtt_client, topic);
+    ASSERT_EQ(ret, 1);
+}
+
+TEST_F(mqtt_client_fixture, invalid_subscribe)
+{
+    int ret; 
+    ret = MQTT_subscribe(mqtt_client, NULL);
+    ASSERT_EQ(ret, 0);
+}
